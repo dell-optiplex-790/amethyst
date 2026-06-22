ce2191508360b6955fd5c06a587eb278faafc42c
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2026-06-22 16:46:53 +0200

    revert build tool

--- initramfs.js
+++ initramfs.js
@@ -3,13 +3,13 @@ var path = require('path');
 if(!fs.existsSync('config.json')) {
     fs.copyFileSync('defaults.json', 'config.json');
 }
-var files = fs.readdirSync('initramfs', {recursive: true, withFileTypes: true}).filter(e => e.isFile()).map(e => path.join(e.parentPath, e.name));
+var files = fs.readdirSync('initramfs', {recursive: true, withFileTypes: true}).filter(e => e.isFile()).map(e => e.name);
 var rfs = {};
 function Uint8Str(uint8) {
     return Array.from(uint8).map(e => e.toString(16)).join(';');
 }
 for(var i = 0; i < files.length; i++) {
-    rfs[files[i]] = Uint8Str(new Uint8Array(fs.readFileSync(files[i])));
+    rfs[files[i]] = Uint8Str(new Uint8Array(fs.readFileSync('initramfs/' + files[i])));
 }
 fs.writeFileSync('temp/ramfs.js', 'export var ramfs = ' + JSON.stringify(rfs));
 fs.writeFileSync('.osbuild', (parseInt(fs.readFileSync('.osbuild', 'utf8')) + 1).toString(), 'utf8');
